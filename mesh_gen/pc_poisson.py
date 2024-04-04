c5393909ea7928553decaab617cc29d3ccc50772
BobWu1998 <bobwu1060@gmail.com>
2024-04-03 22:03:46 -0400

    finished poisson surface recon

--- mesh_gen/pc_poisson.py
+++ mesh_gen/pc_poisson.py
@@ -1,2 +1,47 @@
-# create_from_point_cloud_poisson
+import open3d as o3d
+from os.path import join
+import numpy as np
+import matplotlib.pyplot as plt
+
+def poisson_surface_recon(path_dataset, intrinsic, config, args):
+    pcd = o3d.io.read_point_cloud(join(path_dataset, config["template_global_pc"]))
+    pcd = pcd.voxel_down_sample(voxel_size=args.poisson_voxel_size)
+    
+    pcd.normals = o3d.utility.Vector3dVector(np.zeros((1, 3)))
+    pcd.estimate_normals()
+
+    pcd.orient_normals_consistent_tangent_plane(20)
+    
+    with o3d.utility.VerbosityContextManager(
+            o3d.utility.VerbosityLevel.Debug) as cm:
+        mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
+            pcd, depth=9)
+        
+    densities = np.asarray(densities)
+    density_colors = plt.get_cmap('plasma')(
+        (densities - densities.min()) / (densities.max() - densities.min()))
+    density_colors = density_colors[:, :3]
+    density_mesh = o3d.geometry.TriangleMesh()
+    density_mesh.vertices = mesh.vertices
+    density_mesh.triangles = mesh.triangles
+    density_mesh.triangle_normals = mesh.triangle_normals
+    density_mesh.vertex_colors = o3d.utility.Vector3dVector(density_colors)
+    # o3d.visualization.draw_geometries([density_mesh])
+    
+    vertices_to_remove = densities < np.quantile(densities, 0.01)
+    mesh.remove_vertices_by_mask(vertices_to_remove)
+    
+    o3d.visualization.draw_geometries([mesh])
+        
+def run(config, args):
+    print("integrate the whole RGBD sequence using estimated camera pose.")
+    if config["path_intrinsic"]:
+        intrinsic = o3d.io.read_pinhole_camera_intrinsic(
+            config["path_intrinsic"])
+    else:
+        intrinsic = o3d.camera.PinholeCameraIntrinsic(
+            o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
+    poisson_surface_recon(config["path_dataset"], intrinsic, config, args)
+    
+
 
